42316a559e8464e109bebf98fd11196e956de0a5
codeme254 <otwomadennis365@gmail.com>
2022-12-14 11:02:09 +0300

    insert right 1

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,17 +11,16 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+	binary_tree_t *current_right;
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_node;
 
 	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
-
-	binary_tree_t *current_right;
 
 	current_right = parent->right;
 
