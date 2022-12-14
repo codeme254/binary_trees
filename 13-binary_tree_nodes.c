2c4207946a51f3c6336a1f2ac8bea3a2874f1cb6
codeme254 <otwomadennis365@gmail.com>
2022-12-14 13:11:31 +0300

    count nodes 2

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - count the number of nodes in a binary tree
+ * @tree: pointer to the root node of the tree to count
+ * Return: the number of nodes
+ * if tree is null, return 0
+*/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
